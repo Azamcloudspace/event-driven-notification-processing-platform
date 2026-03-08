d507f19b4dfbecc476b5bb4624b52c430f0859c4
Azamcloudspace <azamiheme19@gmail.com>
2026-03-08 17:11:42 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,36 @@
+import json
+import boto3
+import os
+
+sns = boto3.client("sns")
+TOPIC_ARN = os.environ["TOPIC_ARN"]
+
+def lambda_handler(event, context):
+
+    detail = event.get("detail", {})
+
+    bucket = detail.get("bucket", {}).get("name")
+    key = detail.get("object", {}).get("key")
+    size = detail.get("object", {}).get("size", 0)
+    event_time = event.get("time")
+    event_name = detail.get("reason", "Object Event")
+
+    message = {
+        "bucket": bucket,
+        "file_name": key,
+        "file_size": size,
+        "event_time": event_time,
+        "event_type": event_name
+    }
+
+    sns.publish(
+        TopicArn=TOPIC_ARN,
+        Message=json.dumps(message),
+        Subject="S3 File Upload Event"
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Notification sent")
+    }
 
